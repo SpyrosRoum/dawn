9f7a067ff10b41d6cf78798b8849de049adb3f34
Spyros Roum <spyros.roum@posteo.net>
2025-03-15 18:56:53 +0200

    Ready. Set. GO!

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dawn/dawn
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dawn/dawn_test.clj
@@ -0,0 +1,8 @@
+(ns dawn.dawn-test
+  (:require
+   [clojure.test :refer :all]
+   [dawn.dawn :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

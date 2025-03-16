7b764a2dba8f9cf3b6b5c7e1e95dd146fd66d7aa
Spyros Roum <spyros.roum@posteo.net>
2025-03-16 14:39:56 +0200

    http-kit base

--- src/dawn/dawn.clj
+++ src/dawn/dawn.clj
@@ -1,12 +1,28 @@
 (ns dawn.dawn
+  (:require
+   [org.httpkit.server :as kit]
+   [ring.middleware.defaults :refer [wrap-defaults]]
+   [taoensso.telemere :as tel])
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(def channels (atom #{}))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+(defn on-open    [ch]             (swap! channels conj ch))
+(defn on-close   [ch status-code] (swap! channels disj ch))
+(defn on-receive [ch message]
+  (doseq [ch @channels]
+    (kit/send! ch (str "Message: " message))))
+
+(defn handler [ring-req]
+  (if-not (:websocket? ring-req)
+    {:status 200 :headers {"content-type" "text/html"} :body "Connect WebSockets to this URL."}
+    (kit/as-channel ring-req
+                    {:on-open    #'on-open
+                     :on-receive #'on-receive
+                     :on-close   #'on-close})))
+
+(def app (wrap-defaults #'handler {:websocket {:keepalive true}}))
+
+(comment
+  (def server (kit/run-server #'app {:port 8080}))
+  ,)
